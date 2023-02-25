a12b335844a66c9cd2bf987a93fa903e26449130
Pavel Spitsin <pavel.spitsin@yandex.ru>
2023-02-25 13:18:16 +0300

    [xrGame] Исправлена работа с очередью клиентских скриптовых эвентов.

--- src/xrGame/Level.h
+++ src/xrGame/Level.h
@@ -443,8 +443,8 @@ public:
 private:
     xr_deque<NET_Packet> script_client_events;
 public:
-    NET_Packet* GetLastClientScriptEvent();
-    void PopLastClientScriptEvent();
+    NET_Packet* GetFrontClientScriptEvent();
+    void PopFrontClientScriptEvent();
     u32 GetSizeClientScriptEvent();
 
 #ifdef DEBUG
