xray-omp/omp-engine

[xrGame] фикс некоторых предупреждений компилятора

src/xrGame/console_commands_mp.cpp
@@ -1212,7 +1212,7 @@ public:
 			exclude_raid_from_args(args, tmp_dest, sizeof(tmp_dest));
 			if (xr_strlen(tmp_dest))
 			{
-				sscanf_s(tmp_dest, "%s", filter_string);
+				sscanf_s(tmp_dest, "%s", filter_string, sizeof(filter_string));
 				tmp_functor.filter_string = filter_string;
 			}
 		}
@@ -1295,7 +1295,7 @@ public:
 		exclude_raid_from_args(args, tmp_dest, sizeof(tmp_dest));
 		if (xr_strlen(tmp_dest))
 		{
-			sscanf_s(tmp_dest, "%s", filter_dest);
+			sscanf_s(tmp_dest, "%s", filter_dest, sizeof(filter_dest));
 		}
 		tmp_sv_game->PrintBanList(filter_dest);
 		Level().Server->Print_Banned_Addreses();

Standoff2bot/xray-16

src/xrGame/console_commands_mp.cpp
@@ -1212,7 +1212,7 @@ public:
             exclude_raid_from_args(args, tmp_dest, sizeof(tmp_dest));
             if (xr_strlen(tmp_dest))
             {
-                sscanf(tmp_dest, "%s", filter_string);
+                sscanf(tmp_dest, "%s", filter_string, sizeof(filter_string));
                 tmp_functor.filter_string = filter_string;
             }
         }
@@ -1309,7 +1309,7 @@ public:
         exclude_raid_from_args(args, tmp_dest, sizeof(tmp_dest));
         if (xr_strlen(tmp_dest))
         {
-            sscanf(tmp_dest, "%s", filter_dest);
+            sscanf(tmp_dest, "%s", filter_dest, sizeof(filter_dest));
         }
         tmp_sv_game->PrintBanList(filter_dest);
         Level().Server->Print_Banned_Addreses();