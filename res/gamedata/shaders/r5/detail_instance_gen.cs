aa1bf545585034202191fdd2a537894839463ab9
yohjimane <nguyen.h.alexander@gmail.com>
2026-02-10 12:00:07 -0800

    Match c++ dither logic in instance gen shader

--- res/gamedata/shaders/r5/detail_instance_gen.cs
+++ res/gamedata/shaders/r5/detail_instance_gen.cs
@@ -123,24 +123,15 @@ float pcg_randF(inout PCGState rng, float min_val, float max_val)
 
 float pcg_randFs(inout PCGState rng, float range) { return pcg_randF(rng, -range, range); }
 
-static const int g_dither_matrix[16][16] = {
-    {0,192,48,240,12,204,60,252,3,195,51,243,15,207,63,255},
-    {128,64,176,112,140,76,188,124,131,67,179,115,143,79,191,127},
-    {32,224,16,208,44,236,28,220,35,227,19,211,47,239,31,223},
-    {160,96,144,80,172,108,156,92,163,99,147,83,175,111,159,95},
-    {8,200,56,248,4,196,52,244,11,203,59,251,7,199,55,247},
-    {136,72,184,120,132,68,180,116,139,75,187,123,135,71,183,119},
-    {40,232,24,216,36,228,20,212,43,235,27,219,39,231,23,215},
-    {168,104,152,88,164,100,148,84,171,107,155,91,167,103,151,87},
-    {2,194,50,242,14,206,62,254,1,193,49,241,13,205,61,253},
-    {130,66,178,114,142,78,190,126,129,65,177,113,141,77,189,125},
-    {34,226,18,210,46,238,30,222,33,225,17,209,45,237,29,221},
-    {162,98,146,82,174,110,158,94,161,97,145,81,173,109,157,93},
-    {10,202,58,250,6,198,54,246,9,201,57,249,5,197,53,245},
-    {138,74,186,122,134,70,182,118,137,73,185,121,133,69,181,117},
-    {42,234,26,218,38,230,22,214,41,233,25,217,37,229,21,213},
-    {170,106,154,90,166,102,150,86,169,105,153,89,165,101,149,85}
-};
+static const int g_magic4x4[4][4] = {{0,14,3,13},{11,5,8,6},{12,2,15,1},{7,9,4,10}};
+
+int bwdithermap(uint col, uint row)
+{
+    uint i = col % 4, j = row % 4;
+    uint k = col / 4, l = row / 4;
+    const float magicfact = (255.0 - 1.0) / 16.0;
+    return int(0.5 + g_magic4x4[i][j] * magicfact + (g_magic4x4[k][l] / 16.0) * magicfact);
+}
 
 float Interpolate(float base[4], uint x, uint y, uint size)
 {
@@ -171,7 +162,7 @@ bool InterpolateAndDither(float alpha255[4], uint x, uint y, uint shift_x, uint 
     uint row = (y + shift_z) % 16u;
     uint col = (x + shift_x) % 16u;
 
-    return c > g_dither_matrix[row][col];
+    return c > bwdithermap(col, row);
 }
 
 [numthreads(64, 1, 1)]
@@ -294,8 +285,8 @@ void main(uint3 group_id : SV_GroupID, uint3 thread_id : SV_GroupThreadID)
         else
         {
             DetailModelGPU mdl = g_detail_models[object_id];
-            float scale = pcg_randF(r_scale, mdl.minScale * 0.5, mdl.maxScale * 0.9) * g_detail_height_multiplier;
-            float rotation = pcg_randF(r_yaw, 0.0, 6.28318530718);
+            float scale = mdl.maxScale * g_detail_height_multiplier;
+            float rotation = 0.0f;
 
             uint flags = asuint(mdl.flags);
             const uint DO_NO_WAVING = 0x0001;
