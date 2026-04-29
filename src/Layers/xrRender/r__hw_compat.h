cb3bb471ce66f2045f2264e5afac46575c562e8d
yohjimane <nguyen.h.alexander@gmail.com>
2026-04-29 14:39:47 -0700

    xrRender: CompatHW::hq_skinning() returns true (regression fix)

--- src/Layers/xrRender/r__hw_compat.h
+++ src/Layers/xrRender/r__hw_compat.h
@@ -3,6 +3,6 @@
 namespace xray::render::fg::CompatHW
 {
 inline bool ffp()                            { return false; }
-inline bool hq_skinning()                    { return false; }
+inline bool hq_skinning()                    { return true; }
 inline void shader_option_skinning(int) {}
 }
