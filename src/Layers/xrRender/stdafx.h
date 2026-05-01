d56634adc8ffb20a5602cf4dd1d49422afba9bec
yohjimane <nguyen.h.alexander@gmail.com>
2026-04-30 18:57:54 -0700

    xrRender: encapsulate D3D11/D3DCompiler/dxgi includes in xrRenderDX11/CommonTypes.h

--- src/Layers/xrRender/stdafx.h
+++ src/Layers/xrRender/stdafx.h
@@ -10,24 +10,6 @@
 
 #include "xrParticles/psystem.h"
 
-#include <D3D11.h>
-#include <D3DCompiler.h>
-
-#if __has_include(<dxgi1_4.h>)
-#include <dxgi1_4.h>
-#define HAS_DXGI1_4
-#endif
-
-#if __has_include(<d3d11_2.h>)
-#include <d3d11_2.h>
-#define HAS_DX11_2
-#endif
-
-#if __has_include(<d3d11_3.h>)
-#include <d3d11_3.h>
-#define HAS_DX11_3
-#endif
-
 #pragma warning(push, 0)
 #include <nvrhi/nvrhi.h>
 #pragma warning(pop)
